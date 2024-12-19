d54e7cfdbd55fe433ea1e6b0ed8c242bc3c9dad2
llNight1ll <sami.hmida94@gmail.com>
2024-12-19 09:34:12 +0100

    classe des formes + gravité + game over

--- Form1.cs
+++ Form1.cs
@@ -7,15 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tetris_C_.Shapes;
 
 namespace Tetris_C_
 {
     public partial class Form1 : Form
     {
+        Shape currentShape;
+        Timer timer = new Timer();
         public Form1()
         {
             InitializeComponent();
+
+            loadCanvas();
+
+            currentShape = getRandomShapeWithCenterAligned();
+
+            timer.Tick += Timer_Tick;
+            timer.Interval = 500;
+            timer.Start();
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var isMoveSuccess = moveShapeIfPossible(moveDown: 1);
+
+            // if shape reached bottom or touched any other shapes
+            if (!isMoveSuccess)
+            {
+                // copy working image into canvas image
+                canvasBitmap = new Bitmap(workingBitmap);
+
+                updateCanvasDotArrayWithCurrentShape();
+
+                // get next shape
+                currentShape = getRandomShapeWithCenterAligned();
+            }
+        }
+        Bitmap canvasBitmap;
+        Graphics canvasGraphics;
+        int canvasWidth = 15;
+        int canvasHeight = 20;
+        int[,] canvasDotArray;
+        int dotSize = 20;
+
+        private void loadCanvas()
+        {
+            // Resize the picture box based on the dotsize and canvas size
+            pictureBox1.Width = canvasWidth * dotSize;
+            pictureBox1.Height = canvasHeight * dotSize;
+
+            // Create Bitmap with picture box's size
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+
+            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+
+            // Load bitmap into picture box
+            pictureBox1.Image = canvasBitmap;
+
+            // Initialize canvas dot array. by default all elements are zero
+            canvasDotArray = new int[canvasWidth, canvasHeight];
+        }
+
+        int currentX;
+        int currentY;
+
+        private Shape getRandomShapeWithCenterAligned()
+        {
+            var shape = ShapesHandler.GetRandomShape();
+
+            // Calculate the x and y values as if the shape lies in the center
+            currentX = 7;
+            currentY = -shape.Height;
+
+            return shape;
+        }
+
+
+        // returns if it reaches the bottom or touches any other blocks
+        private bool moveShapeIfPossible(int moveDown = 0, int moveSide = 0)
+        {
+            var newX = currentX + moveSide;
+            var newY = currentY + moveDown;
+
+            // check if it reaches the bottom or side bar
+            if (newX < 0 || newX + currentShape.Width > canvasWidth
+                || newY + currentShape.Height > canvasHeight)
+                return false;
+
+            // check if it touches any other blocks 
+            for (int i = 0; i < currentShape.Width; i++)
+            {
+                for (int j = 0; j < currentShape.Height; j++)
+                {
+                    if (newY + j > 0 && canvasDotArray[newX + i, newY + j] == 1 && currentShape.Dots[j, i] == 1)
+                        return false;
+                }
+            }
+
+            currentX = newX;
+            currentY = newY;
+
+            drawShape();
+
+            return true;
+        }
+
+        Bitmap workingBitmap;
+        Graphics workingGraphics;
+
+        private void drawShape()
+        {
+            workingBitmap = new Bitmap(canvasBitmap);
+            workingGraphics = Graphics.FromImage(workingBitmap);
+
+            for (int i = 0; i < currentShape.Width; i++)
+            {
+                for (int j = 0; j < currentShape.Height; j++)
+                {
+                    if (currentShape.Dots[j, i] == 1)
+                        workingGraphics.FillRectangle(Brushes.Black, (currentX + i) * dotSize, (currentY + j) * dotSize, dotSize, dotSize);
+                }
+            }
+
+            pictureBox1.Image = workingBitmap;
+        }
+
+
+        private void updateCanvasDotArrayWithCurrentShape()
+        {
+            for (int i = 0; i < currentShape.Width; i++)
+            {
+                for (int j = 0; j < currentShape.Height; j++)
+                {
+                    if (currentShape.Dots[j, i] == 1)
+                    {
+                        checkIfGameOver();
+
+                        canvasDotArray[currentX + i, currentY + j] = 1;
+                    }
+                }
+            }
+        }
+
+        private void checkIfGameOver()
+        {
+            if (currentY < 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Game Over");
+                Application.Restart();
+            }
+        }
+
+
+
 
     }
 }
---
+++ Shapes/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_C_.Shapes
+{
+    internal class Shape
+    {
+        public int Width;
+        public int Height;
+        public int[,] Dots;
+    }
+}
