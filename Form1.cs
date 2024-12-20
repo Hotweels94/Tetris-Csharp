e287ed1638472132dc08e7762e51b9f77b849e44
Your Name <you@example.com>
2024-12-20 11:52:05 +0100

    couleur finie + correction bug lors des clear des lignes

--- Form1.cs
+++ Form1.cs
@@ -32,6 +32,49 @@ namespace Tetris_C_
             this.KeyDown += Form1_KeyDown;
         }
 
+        Bitmap canvasBitmap;
+        Graphics canvasGraphics;
+        int canvasWidth = 15;
+        int canvasHeight = 20;
+        Brush[,] canvasDotArray;
+
+        int dotSize = 20;
+        private void loadCanvas()
+        {
+            pictureBox1.Width = canvasWidth * dotSize;
+            pictureBox1.Height = canvasHeight * dotSize;
+
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+            pictureBox1.Image = canvasBitmap;
+
+            canvasDotArray = new Brush[canvasWidth, canvasHeight];
+            for (int i = 0; i < canvasWidth; i++)
+            {
+                for (int j = 0; j < canvasHeight; j++)
+                {
+                    canvasDotArray[i, j] = Brushes.LightGray;
+                }
+            }
+        }
+
+
+        int currentX;
+        int currentY;
+        private Shape getRandomShapeWithCenterAligned()
+        {
+            var shape = ShapesHandler.GetRandomShape();
+
+            // Calculate the x and y values as if the shape lies in the center
+            currentX = 7;
+            currentY = -shape.Height;
+
+            return shape;
+        }
+
+        Bitmap workingBitmap;
+        Graphics workingGraphics;
         private void Timer_Tick(object sender, EventArgs e)
         {
             var isMoveSuccess = moveShapeIfPossible(moveDown: 1);
@@ -51,78 +94,74 @@ namespace Tetris_C_
                 clearFilledRowsAndUpdateScore();
             }
         }
-        Bitmap canvasBitmap;
-        Graphics canvasGraphics;
-        int canvasWidth = 15;
-        int canvasHeight = 20;
-        int[,] canvasDotArray;
-        int dotSize = 20;
 
-        private void loadCanvas()
+        private void updateCanvasDotArrayWithCurrentShape()
         {
-            // Resize the picture box based on the dotsize and canvas size
-            pictureBox1.Width = canvasWidth * dotSize;
-            pictureBox1.Height = canvasHeight * dotSize;
-
-            // Create Bitmap with picture box's size
-            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            canvasGraphics = Graphics.FromImage(canvasBitmap);
-
-            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
-
-            // Load bitmap into picture box
-            pictureBox1.Image = canvasBitmap;
-
-            // Initialize canvas dot array. by default all elements are zero
-            canvasDotArray = new int[canvasWidth, canvasHeight];
-        }
-
-        int currentX;
-        int currentY;
-
-        private Shape getRandomShapeWithCenterAligned()
-        {
-            var shape = ShapesHandler.GetRandomShape();
-
-            // Calculate the x and y values as if the shape lies in the center
-            currentX = 7;
-            currentY = -shape.Height;
-
-            return shape;
-        }
-
-
-        // returns if it reaches the bottom or touches any other blocks
-        private bool moveShapeIfPossible(int moveDown = 0, int moveSide = 0)
-        {
-            var newX = currentX + moveSide;
-            var newY = currentY + moveDown;
-
-            // check if it reaches the bottom or side bar
-            if (newX < 0 || newX + currentShape.Width > canvasWidth
-                || newY + currentShape.Height > canvasHeight)
-                return false;
-
-            // check if it touches any other blocks 
             for (int i = 0; i < currentShape.Width; i++)
             {
                 for (int j = 0; j < currentShape.Height; j++)
                 {
-                    if (newY + j > 0 && canvasDotArray[newX + i, newY + j] == 1 && currentShape.Dots[j, i] == 1)
-                        return false;
+                    if (currentShape.Dots[j, i] == 1)
+                    {
+                        checkIfGameOver();
+
+                        // Save the color of the block
+                        canvasDotArray[currentX + i, currentY + j] = currentShape.Color;
+                    }
                 }
             }
-
-            currentX = newX;
-            currentY = newY;
-
-            drawShape();
-
-            return true;
         }
 
-        Bitmap workingBitmap;
-        Graphics workingGraphics;
+
+        private void checkIfGameOver()
+        {
+            if (currentY < 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Game Over");
+                Application.Restart();
+            }
+        }
+
+        // returns if it reaches the bottom or touches any other blocks
+        private bool moveShapeIfPossible(int moveDown = 0, int moveSide = 0)
+{
+    var newX = currentX + moveSide;
+    var newY = currentY + moveDown;
+
+    //Verify is the shape is out of the grid
+    if (newX < 0 || newX + currentShape.Width > canvasWidth
+        || newY + currentShape.Height > canvasHeight)
+        return false;
+
+    //Verify if the sahpe touch other shape
+    for (int i = 0; i < currentShape.Width; i++)
+    {
+        for (int j = 0; j < currentShape.Height; j++)
+        {
+            if (currentShape.Dots[j, i] == 1) //IF this dot is appart of the shape
+            {
+                int targetX = newX + i;
+                int targetY = newY + j;
+
+                //Verify that the space is blank
+                if (targetY >= 0 && canvasDotArray[targetX, targetY] != Brushes.LightGray)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+ 
+    currentX = newX;
+    currentY = newY;
+
+    drawShape();
+
+    return true;
+}
+
 
         private void drawShape()
         {
@@ -141,125 +180,132 @@ namespace Tetris_C_
             pictureBox1.Image = workingBitmap;
         }
 
-
-        private void updateCanvasDotArrayWithCurrentShape()
-        {
-            for (int i = 0; i < currentShape.Width; i++)
-            {
-                for (int j = 0; j < currentShape.Height; j++)
-                {
-                    if (currentShape.Dots[j, i] == 1)
-                    {
-                        checkIfGameOver();
-
-                        canvasDotArray[currentX + i, currentY + j] = 1;
-                    }
-                }
-            }
-        }
-
-        private void checkIfGameOver()
-        {
-            if (currentY < 0)
-            {
-                timer.Stop();
-                MessageBox.Show("Game Over");
-                Application.Restart();
-            }
-        }
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             var verticalMove = 0;
             var horizontalMove = 0;
 
+            // calculate the vertical and horizontal move values
+            // based on the key pressed
             switch (e.KeyCode)
             {
+                // move shape left
                 case Keys.Left:
                     verticalMove--;
                     break;
 
+                // move shape right
                 case Keys.Right:
                     verticalMove++;
                     break;
 
+                // move shape down faster
                 case Keys.Down:
                     horizontalMove++;
                     break;
 
+                // rotate the shape clockwise
                 case Keys.Up:
                     currentShape.turn();
                     break;
-
                 default:
                     return;
             }
 
             var isMoveSuccess = moveShapeIfPossible(horizontalMove, verticalMove);
 
+            // if the player was trying to rotate the shape, but
+            // that move was not possible - rollback the shape
             if (!isMoveSuccess && e.KeyCode == Keys.Up)
                 currentShape.rollback();
         }
 
         int score;
-
-        public void clearFilledRowsAndUpdateScore()
+       public void clearFilledRowsAndUpdateScore()
         {
-            for (int i = 0; i < canvasHeight; i++)
+            for (int i = canvasHeight - 1; i >= 0; i--)
             {
-                int j;
-                for (j = canvasWidth - 1; j >= 0; j--)
+                bool isRowFull = true;
+
+                // Verify is a line is completed
+                for (int j = 0; j < canvasWidth; j++)
                 {
-                    if (canvasDotArray[j, i] == 0)
+                    if (canvasDotArray[j, i] == Brushes.LightGray)
                     {
+                        isRowFull = false;
                         break;
                     }
                 }
-                if (j == -1)
+
+                //Delet a complet line
+                if (isRowFull)
                 {
                     score++;
                     label1.Text = "Score: " + score;
-                    label2.Text = "Level: " + score / 10;
-                    timer.Interval -= 10;
+                    label2.Text = "Level: " + (score / 10);
 
-                    for (j = 0; j < canvasWidth; j++)
+                    timer.Interval = Math.Max(50, timer.Interval - 10);
+
+                    // Make all the above lines down from 1 block
+                    for (int k = i; k > 0; k--)
                     {
-                        for (int k = i; k > 0; k--)
+                        for (int j = 0; j < canvasWidth; j++)
                         {
                             canvasDotArray[j, k] = canvasDotArray[j, k - 1];
                         }
-
-                        canvasDotArray[j, 0] = 0;
                     }
+
+                    // Errase the first line
+                    for (int j = 0; j < canvasWidth; j++)
+                    {
+                        canvasDotArray[j, 0] = Brushes.LightGray;
+                    }
+
+                    i++; // Verify the if the grid after the clear
                 }
             }
-            
+
+            redrawCanvas();
+        }
+
+
+        private void redrawCanvas()
+        {
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+
+            //Clear the canvas
+            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+
+            // Draw each bloc of each sahpe with his color
             for (int i = 0; i < canvasWidth; i++)
             {
                 for (int j = 0; j < canvasHeight; j++)
                 {
-                    canvasGraphics = Graphics.FromImage(canvasBitmap);
-                    canvasGraphics.FillRectangle(
-                        canvasDotArray[i, j] == 1 ? Brushes.Transparent : Brushes.LightGray,
-                        i * dotSize, j * dotSize, dotSize, dotSize
-                        );
+                    if (canvasDotArray[i, j] != Brushes.LightGray) // If there is a block
+                    {
+                        canvasGraphics.FillRectangle(canvasDotArray[i, j], i * dotSize, j * dotSize, dotSize, dotSize);
+                    }
                 }
             }
+
             pictureBox1.Image = canvasBitmap;
         }
 
+
+
         Bitmap nextShapeBitmap;
         Graphics nextShapeGraphics;
-
         private Shape getNextShape()
         {
             var shape = getRandomShapeWithCenterAligned();
 
+            // Codes to show the next shape in the side panel
             nextShapeBitmap = new Bitmap(6 * dotSize, 6 * dotSize);
             nextShapeGraphics = Graphics.FromImage(nextShapeBitmap);
 
             nextShapeGraphics.FillRectangle(Brushes.LightGray, 0, 0, nextShapeBitmap.Width, nextShapeBitmap.Height);
 
+            // Find the ideal position for the shape in the side panel
             var startX = (6 - shape.Width) / 2;
             var startY = (6 - shape.Height) / 2;
 
